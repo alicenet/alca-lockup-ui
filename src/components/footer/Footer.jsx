bf1a1b221c9a6e414d24dce18c4a4cb48829b241
Adrian Bona <adrianbona@outlook.com>
2022-08-17 12:51:25 -0300

    OCD Commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,12 +6,22 @@ const TWITTER_URL = process.env.REACT_APP_TWITTER_URL;
 const DISCORD_URL = process.env.REACT_APP_DISCORD_URL;
 
 export function Footer() {
+
     return (
+
         <div className="fixed left-0 bottom-0 w-full bg-white flex p-4 justify-between">
             <div>
-                Follow us on: {" "}
-                <Icon name="twitter" className="cursor-pointer" onClick={() => window.open(TWITTER_URL, '_blank').focus()} />
-                <Icon name="discord" className="cursor-pointer" onClick={() => window.open(DISCORD_URL, '_blank').focus()} />
+                Follow us on:&nbsp;
+                <Icon
+                    name="twitter"
+                    className="cursor-pointer"
+                    onClick={() => window.open(TWITTER_URL, '_blank').focus()}
+                />
+                <Icon
+                    name="discord"
+                    className="cursor-pointer"
+                    onClick={() => window.open(DISCORD_URL, '_blank').focus()}
+                />
             </div>
             <div className="cursor-pointer gap-6 hidden md:flex">
                 <Link to="/about">About</Link>
@@ -20,5 +30,7 @@ export function Footer() {
                 <Link to="/">Alicenet Inc &copy; 2022</Link>
             </div>
         </div>
-    )
+
+    );
+
 }
