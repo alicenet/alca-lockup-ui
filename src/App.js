b1c7194c4cd7c7ccaa4e404564679ac8f063a93f
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-08-22 13:27:05 -0400

    main update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
-import { Footer, Header, ActionTabs, About, TOS } from 'components';
-
 function App() {
-
-    const DefaultRoutes = () => {
-        return (<>
-            <Route exact path="/" element={<ActionTabs/>} />
-            <Route exact path="/about" element={<About/>} />
-            <Route exact path="/tos" element={<TOS/>} />
-
-        </>)
-    }
 
     return (
 
         <Container fluid>
-
-            <BrowserRouter>
-
-                <Header />
-                <Routes>
-                    {DefaultRoutes()}
-                </Routes>
-                <Footer/>
-
-            </BrowserRouter>
-
+            alca.alice.net
         </Container>
 
     );
