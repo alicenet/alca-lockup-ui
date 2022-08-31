6f574ba0478f89fa8a19ebe0f387f07c346233ca
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-08-31 15:47:59 -0400

    Final touches

--- src/components/balanceStatus/BalanceStatus.jsx
+++ src/components/balanceStatus/BalanceStatus.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Segment, Label, } from "semantic-ui-react";
+import { Segment, Label, Loader, } from "semantic-ui-react";
 
 export function BalanceStatus({ row }) {
     const { balances, loading } = useSelector(state => (
@@ -13,7 +13,7 @@ export function BalanceStatus({ row }) {
     const BalanceLabel = ({ balanceType }) => (
         <div size className="flex flex-col justify-start items-start ml-2 mr-2 h-10 text-left">
             <div className="text-gray-500 text-xs font-bold">Current {balanceType.toUpperCase()} Balance</div>
-            <div className="text-gray-700 text-xl mt-1">{loading ? "" : Number(balances[balanceType]).toLocaleString(false, { maximumFractionDigits: 4 })} </div>
+            <div className="text-gray-700 text-xl mt-1">{loading ? <Loader size="small" active/> : Number(balances[balanceType]).toLocaleString(false, { maximumFractionDigits: 4 })} </div>
         </div>
     )
 
