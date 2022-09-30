e40289d142a44597444a222fda5ad31f1d9a0a11
ZJ <zijunlin32@gmail.com>
2022-09-30 10:42:11 -0400

    removed unused functions

--- src/components/unlock/Unlock.jsx
+++ src/components/unlock/Unlock.jsx
@@ -24,88 +24,13 @@ export function Unlock() {
 
     //figure out how to update this
     //user's staked amount
-    const [stakeAmt, setStakeAmt] = React.useState("");
-    const [waiting, setWaiting] = React.useState(false);
+    
+   
     const [status, setStatus] = React.useState({});
-    const [allowanceMet, setAllowanceMet] = React.useState(false);
+   
     const [hash, setHash] = React.useState("");
 
-    React.useEffect( () => {
-        setStakeAmt("")
-    }, [])
 
-    React.useEffect(() => {
-        try {
-            if(!stakeAmt) return;
-            setStatus({});
-            setAllowanceMet(ethers.BigNumber.from(alcaStakeAllowance || 0).gt(ethers.utils.parseUnits(stakeAmt || "0", DECIMALS)));
-            if(ethers.utils.parseUnits(stakeAmt || "0", DECIMALS).gt(ethers.utils.parseUnits(alcaBalance || "0", DECIMALS))) {
-                setStatus({ 
-                    error: true, 
-                    message: "Stake amount higher than current balance"
-                });
-            }
-        } catch (exc) {
-            setStatus({ 
-                error: true, 
-                message: "There was a problem with your input, please verify" 
-            });
-        }
-    // eslint-disable-next-line
-    }, [stakeAmt]);
-
-    const approveStaking = async () => {
-        try {
-            setHash("");
-            setStatus({});
-            setWaiting(true)
-
-            const tx = await ethAdapter.sendStakingAllowanceRequest();
-            await tx.wait();
-
-            setWaiting(false);
-            dispatch(APPLICATION_ACTIONS.updateBalances());
-            setStatus({ 
-                error: false, 
-                message: "Allowance granted to the Staking Contract, you can now stake ALCA" 
-            });
-            setHash(tx?.hash);
-        } catch (exc) {
-            setWaiting(false);
-            setStatus({ 
-                error: true, 
-                message: "There was a problem with your request, please verify or try again later" 
-            });
-        }
-    }
-
-    const stake = async () => {
-        try {
-            setHash("");
-            setStatus({});
-            setWaiting(true)
-
-            const tx = await ethAdapter.openStakingPosition(stakeAmt);
-            const rec = await tx.wait();
-
-            if (rec.transactionHash) {
-                console.log("hit 1");
-                await dispatch(APPLICATION_ACTIONS.updateBalances(TOKEN_TYPES.ALL));
-                console.log("hit 2");
-                setStatus({ error: false, message: "Stake completed" });
-                setHash(rec.transactionHash);
-                // setStakeAmt(""); // Was resetting a UI element to the user
-                setWaiting(false);
-            }
-        } catch (exc) {
-            console.log('uhoh')
-            setWaiting(false);
-            setStatus({ 
-                error: true, 
-                message: "There was a problem with your request, please verify or try again later" 
-            });
-        }
-    }
 
     const UnlockHeader = () => {
         if(!status?.message || status.error) {
@@ -153,9 +78,6 @@ export function Unlock() {
                                 content={
                                     "Unlock Positions"
                                 }
-                                onClick={allowanceMet ? stake : approveStaking}
-                                disabled={!stakeAmt || status?.error}
-                                loading={waiting}
                             />      
                         </div>
                     </>
